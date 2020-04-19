ea3f8d032594605467531fefeaa312a7724b4c0d
pjmantoss <joelptoss@gmail.com>
2020-04-19 15:55:37 +0100

    update script

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid');
     const width = 10
     const height = 10
+
+    //Tetrominoes
+    const lTetromino = [
+        [1, width+1, width*2+1, 2],
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1, width*2],
+        [width, width*2, width*2+1, width*2+2]
+    ]
+
+
 })
