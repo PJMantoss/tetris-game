fc22b756d6d4ad90c3003172f2f77c454f464f59
pjmantoss <joelptoss@gmail.com>
2020-04-19 16:49:00 +0100

    add images

--- script.js
+++ script.js
@@ -17,11 +17,11 @@ document.addEventListener('DOMContentLoaded', () => {
     let nextRandom = 0;
 
     const colors = [
-        "url(images/)",
-        "url(images/)",
-        "url(images/)",
-        "url(images/)",
-        "url(images/)"
+        "url(images/blue_block.png)",
+        "url(images/pink_block.png)",
+        "url(images/purple_block.png)",
+        "url(images/peach_block.png)",
+        "url(images/yellow_block.png)"
     ]
 
     //Tetrominoes
